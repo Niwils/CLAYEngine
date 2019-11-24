01b90a52e9d9eb1b10166851605d0942dd3472e1
davelister <davelister@davelister.net>
2019-11-24 10:52:47 +0100

    Added: basic files for display redering + game engine Core (Events and MarketPlace)

---
+++ src/game/ProcessFactory.h
@@ -0,0 +1,26 @@
+/*
+ * The <unnamed> factory builder project.
+ *
+ * \file src/game/ProcessFactory.h
+ *
+ * \brief A ProcessFactory creates an ItemFactory according to the current year,
+ * the available R&D level and solutions proposed by the industrial process industry.
+ */
+
+/*
+ * CHANGELOG:
+ * 23/11/2019: File creation (NG)
+ */
+#include <IToken.h>
+
+class ProcessFactory
+{
+public:
+	ProcessFactory();
+	~ProcessFactory();
+
+private:
+	// TODO add Process type
+	// TODO add required Item or raw materials settings
+	// TODO add resulting Item settings
+};
